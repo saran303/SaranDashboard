742c73f9672e33f4efc18cb47b823ed0a6e6651c
saran303 <saran13051999@gmail.com>
2024-09-19 20:55:48 +0530

    Chnages done

--- dashboard/src/Components/Card/Card.js
+++ dashboard/src/Components/Card/Card.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import styles from './Card.module.css'
 
 const Card = () => {
   return (
-    <div>
-      <div>
+    <div className={styles.card}>
+      <div className={styles.courseWrapper}>
         <div>
           <h3>Course Code</h3>
           <p>BA3102</p>
